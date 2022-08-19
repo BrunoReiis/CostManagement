afe1a203257490f5cf17caa4c2a4725d8d447c0f
BrunoReiis <Bruno.Reis15@gmail.com>
2022-08-19 14:54:25 -0300

    Add date to ExpensiveDate and his css

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,11 @@
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
 function ExpenseItem(propsExpense) {
-    const month = propsExpense.date.toLocaleString('en-US' , { month: 'long'});
-    const day = propsExpense.date.toLocaleString('en-US', {day: '2-digit'});
-    const year = propsExpense.date.getFullYear();
 
   return (
     <div className="expense-item">
-      <div>
-        <div>{day}</div>
-        <div>{month}</div>
-        <div>{year}</div>
-      </div>
+      <ExpenseDate date={propsExpense.date}/>
       <div className="expense-item__description">
         <h2>{propsExpense.title}</h2>
         <div className="expense-item__price">${propsExpense.amount}</div>
